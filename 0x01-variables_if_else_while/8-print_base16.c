3167af6edb2c543c416e2d5c8b95d66f6e1a08d2
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-06 14:55:01 -0700

    C variables if else while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char l;
+	n = 0;
+	
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
