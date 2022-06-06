c7ff3adf912423245121573e378b73a97aa810a2
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-06 06:53:30 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = "abcdefghigklmnopqrstuvwxvz";
+	putchar(ch);
+	return (0);
+}
