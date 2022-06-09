c166727d34b305e0a9b552bc75481bf3ae6aed2e
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-09 15:16:56 -0700

    C more function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10times of numbers
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int t;
+	int num;
+
+	t = 0;
+
+	while (t < 10)
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar(num);
+		_putchar('\n');
+	}
+	t++;
+}
