44f916582f8930ce7beb657049f2598bdcb74e02
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-09 11:36:10 -0700

    C more functions and nested looops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: digit to be checked
+ * Return: 1 if c is a digit and 0 if c is otherwise
+ */
+int _isdigit(int c)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		if (d == c)
+			return (1);
+		else
+			return (0);
+	}
+}
