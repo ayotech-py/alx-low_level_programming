a773ef25e0016d7d9da6a840e4853b78bf9869a9
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-10 00:29:50 -0700

    C more functions and nested loop

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line on the terminal
+ * @n: number of lines
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int t;
+
+	if (n > 0)
+	{
+		for (t = 0; t <= n; t++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
