4eac4829dfce58090edc346a6fbeb7cee9e73071
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-14 12:28:49 -0700

    C pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	*a = &b;
+	*b = &a;
+}
