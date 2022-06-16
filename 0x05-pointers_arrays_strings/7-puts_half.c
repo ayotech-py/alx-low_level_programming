3737f0a16bed9dd0350961068421929e19ab4d9d
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-16 13:49:56 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: strung
+ */
+void puts_half(char *str)
+{
+	int c;
+	int n;
+
+	for (c = 0; str[i] != '\0'; c++)
+	{
+	}
+	if ((c % 2) == 0)
+	{
+		for (n = c / 2; n <= c; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = (c - 1) / 2; n <= c; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
