c5b9ea152f359a96f6942c2cf1e20e3c6b0ab6ab
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-16 12:12:21 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * rev_string - prints string in reverse
+ * @s: string
+ */
 void rev_string(char *s)
 {
 	int i;
@@ -7,7 +11,9 @@ void rev_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		_putchar(s[i]);
 	}
+	_putchar('\n');
 	for (c = i - 1; c != -1; c--)
 		_putchar(s[c]);
 	_putchar('\n');
