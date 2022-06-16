e27ce2128b7a72e2d4b625f88b7a33a8d1e6ca7f
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-16 11:39:48 -0700

    c reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i;
+	int c;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (c = i; c != -1; c--)
+		_putchar(s[c]);
 	_putchar('\n');
-}
 
+}
