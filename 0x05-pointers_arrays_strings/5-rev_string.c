f54ad934bd89fe41afe1a07c79b040862ebb179b
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-16 12:51:01 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s)
 {
 	int i;
 	int c;
+	char *str;
+
+	str = s;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(s[i]);
 	}
-	_putchar('\n');
 	for (c = i - 1; c != -1; c--)
-		_putchar(s[c]);
-	_putchar('\n');
+		*str = s[c];
 }
