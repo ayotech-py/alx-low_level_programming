d054e3441733599c7c200cae31e0514d0ed4f449
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-16 15:13:21 -0700

    reverse

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - Printing arrays
@@ -12,9 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i <= (n - 1); i++)
 	{
 		if (i == (n - 1))
-			printf("%d", arr[i]);
+			printf("%d", a[i]);
 		else
-			printf("%d, ", arr[i]);
+			printf("%d, ", a[i]);
 	}
 	printf("\n");
 }
