570162801e7a42ed937ea7ac19c11642f289d52a
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-18 14:33:13 -0700

    More pointers second task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Function that copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: n bytes
+ * Return: returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count;
+	int count2;
+
+	count = 0;
+	count2 = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+
+	while (count2 <= n)
+	{
+		dest[count] = src[count2];
+		count++;
+		count2++;
+	}
+	return (dest);
+}
+
+
