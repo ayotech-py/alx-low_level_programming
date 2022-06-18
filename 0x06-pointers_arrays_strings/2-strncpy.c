7576e67abc264f7cfbcfe8e8f26e5d9fbc1efb15
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-18 15:12:11 -0700

    More pointers second task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: string 1
+ * @src: string 2
+ * @n: bytes
+ * Return: returns dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		*(dest + count) = *(src + count);
+		count++;
+	}
+	return (dest);
+}
