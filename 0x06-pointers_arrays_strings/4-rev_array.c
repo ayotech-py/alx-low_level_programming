6a4a1f72782fdface6c8707e4af215bb1555bbba
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-19 05:02:09 -0700

    More pointers arrys

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array to be reversed
+ * @n: size of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		a[i] = a[n - 1];
+		i++;
+		n--;
+	}
+	return (a);
+}
+
