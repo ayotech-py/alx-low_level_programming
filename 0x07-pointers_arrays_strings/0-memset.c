b03653d289dcce2a80ffcc6ea1b46a76febe2c6f
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-20 05:23:40 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - functions that fills a buffer
+ * @b: what to fill
+ * @n: bytes
+ * @s: points to a memory
+ * Return: Returns the pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
