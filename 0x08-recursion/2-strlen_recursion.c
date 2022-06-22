422163411d9c266492ac8d436482a27761b4bc9a
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-22 09:27:49 -0700

    length of a sting using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - cal the lenght of a string
+ * @s: string
+ * Return: Returns the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (*s != '\0')
+	{
+		n++;
+		return (n + _strlen_recursion(s + 1));
+	}
+}
