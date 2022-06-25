1f4ab419f6b1dc981887a7bcac8f649121cd894c
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-25 08:34:05 -0700

    c Multiplications

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two arguments
+ * @argc: size of array
+ * @argv: argv string
+ *
+ * Return: returns 1 and 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int mul;
+		int a = strtol(argv[1], NULL, 10);
+		int b = strtol(argv[2], NULL, 10);
+
+		mul = a * b;
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
