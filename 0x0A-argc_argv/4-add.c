faf20670867ce24e0a915a7ac2c21783e44bbf3f
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-25 10:16:03 -0700

    infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add positive numbers
+ * @argc: array size
+ * @argv: array vector
+ * Return: return 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int add;
+
+	if (argc > 1)
+	{
+		count = 1;
+
+		while (count < argc)
+		{
+			if (atoi(argv[count]) < 0)
+			{
+				printf("Error\n");
+				return (1);
+				break;
+			}
+			else
+			{
+				add += atoi(argv[count]);
+				count++;
+			}
+		}
+		printf("%d\n", add);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
