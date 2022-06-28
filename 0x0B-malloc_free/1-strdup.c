26ae5668845cb5f6803fa7e33e2f8216bd7c7083
Ajao Malik <ajaomalik2018@gmail.com>
2022-06-28 11:01:37 -0700

    malloc and free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,13 @@ char *_strdup(char *str)
 	int i, j;
 	char *arr;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 	arr = malloc((i + 1) * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
 	j = 0;
-
 	while (str[j] != '\0')
 	{
 		arr[j] = str[j];
