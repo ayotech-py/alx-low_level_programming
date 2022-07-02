e325e2744edb98cd180b2d2d28afe2cfe4850172
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-02 07:30:39 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory
+ * @nmemb: number
+ * @size: size
+ * Return: returns NULL and Pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *str;
+
+	str = calloc(nmemb, (sizeof(int) * size));
+	if (str == NULL)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	return (str);
+}
