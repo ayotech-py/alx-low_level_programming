60f9ef5fa5d9c04a7aa5fe118b612f8f62f1f6b7
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-04 12:11:10 -0700

    c preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef ABS(x)
-#define ABS(x)
-#if ABS < 1
-#undef ABS
-#define ABS(x) (4)
-#endif
-#else
-#undef ABS
-#define ABS(x) x
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
