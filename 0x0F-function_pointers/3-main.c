1774708fca4f7100254553ed6135369e1040771c
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-08 15:52:55 -0700

    c function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,12 +5,21 @@
 int main(int argc, char **argv)
 {
 	int res;
+	char *op;
+
+	op = argv[2];
 
 	if (argc == 4)
 	{
+		if ((*op == '/' && atoi(argv[3]) == 0) || (*op == '%' && atoi(argv[3]) == 0))
+		{
+			printf("Error\n");
+			exit(99);
+		}
 		res = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 		printf("%d\n", res);
 	}
+
 	else
 		printf("Error\n");
 	return (0);
