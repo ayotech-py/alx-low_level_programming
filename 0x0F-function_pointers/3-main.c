422c9d54c17214d1b037b94cbfb71f71a1849aad
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-08 16:25:58 -0700

    c funtion pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 
 	if (argc == 4)
 	{
-		if ((*op != '+') || (*op != '-') || (*op != '*') || (*op != '/') || (*op != '%'))
+		if ((*op != '+') && (*op != '-') && (*op != '*') && (*op != '/') && (*op != '%'))
 		{
 			printf("Error\n");
 			exit(99);
