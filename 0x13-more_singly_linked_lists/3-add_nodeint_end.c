8bda165f68d1f06b589048051ac87ca01f02d3c6
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-19 12:06:54 -0700

    more singy linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,10 +10,20 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *temp = malloc(sizeof(listint_t));
 
-	if (temp == NULL)
-		return (NULL);
 	temp->n = n;
 	temp->next = NULL;
-	*head = temp;
-	return (*head);
+	if (temp == NULL)
+		return (NULL);
+	if (*head == NULL)
+		*head = temp;
+	else
+	{
+
+		while (*head != NULL)
+		{
+			*head = (*head)->next;
+		}
+		*head = temp;
+	}
+	return (temp);
 }
