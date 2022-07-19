c2ef45adf27baafe97ac94d5300e7cffa69b2353
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-19 16:29:27 -0700

    c singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
-	unsigned int i, j;
+	unsigned int i;
 
 	temp = head;
 	for (i = 0; i < index; i++)
 	{
+		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
-		temp = temp->next;
 	}
 	return (temp);
 }
